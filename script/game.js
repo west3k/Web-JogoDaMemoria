eaf467767bf506d56b8e71584f0e8d16aee412e6
Wesley Oliveira <wesoliveirasilva2@gmail.com>
2020-12-04 17:20:24 -0300

    Feita separação view e lógica, checagem de pares e correção de bugs

---
+++ script/game.js
@@ -0,0 +1,117 @@
+let game = {
+
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    setCard: function(id){
+       let card = this.cards.filter(card => card.id === id)[0]
+       console.log(card)
+        if(card.flipped || this.lockMode){
+            return false
+        }
+        if(!this.firstCard){
+            this.firstCard = card;
+            this.firstCard.flipped = true
+            return true
+        }else{
+            this.secondCard = card;
+            this.secondCard.flipped = true
+            this.lockMode = true
+            return true
+        }
+    },
+
+    checkMatch: function(){
+        if(!this.firstCard || !this.secondCard){
+            return false
+        }
+        return(this.firstCard.icon === this.secondCard.icon)
+    },
+
+    clearCards: function(){
+        this.firstCard = null
+        this.secondCard = null
+        this.lockMode = false
+    },
+
+    unflipCards: function(){
+        this.firstCard.flipped = false
+        this.secondCard.flipped = false
+        this.clearCards()
+    },
+
+    techs: [
+        'bootstrap',
+        'css',
+        'electron',
+        'firebase',
+        'html',
+        'javascript',
+        'jquery',
+        'mongo',
+        'node',
+        'react'
+    ],
+
+    cards: null,
+
+    // modelo para cada carta carta, que deve ter o icone.  ICON
+    // precisa saber se a carta está virada ou não. FLIPPED
+    // diferenciar uma carta da outra, mesmo que sejam iguais, devem ter ids diferentes ID
+
+    //Função criar cartas por tecnologia.
+    createCardsFromTechs: function () {
+        this.cards = [];
+
+        this.techs.forEach((tech) => {
+            this.cards.push(this.createPairFromTech(tech))
+        })
+        this.cards = this.cards.flatMap(pair => pair)
+        this.shuffleCards();
+        return this.cards
+    },
+
+
+    //Função que vai retornar o array com o par de 2 objetos contendo...
+    // ID(RANDOMICO), ICON(tecnologia que o objeto representa) E FLIPPED(Começa sempre false)
+
+
+    createPairFromTech: function (tech) {
+        return [{
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false,
+        },
+
+        {
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false,
+        }]
+    },
+
+    //Função responsável por criar um ID randomico, concatenando o nome da tecnologia
+    //Com um número aleatório inteiro entre 0 e 1 mil
+
+    createIdWithTech: function (tech) {
+        return tech + parseInt(Math.random() * 1000)
+    },
+
+    //Função para embaralhamento das cartas, contendo a lógica
+    //selecionando a ultima carta o index dela, e trocando por outra carta aleatória.
+
+    shuffleCards: function (cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+        }
+    }
+
+}
--- script/script.js
+++ script/script.js
@@ -3,37 +3,20 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-]
-
-let cards = null;
 
 //Função que inicia o jogo acionando a criação das cartas e embaralhamento.
 
 startGame();
 
 function startGame() {
-    cards = createCardsFromTechs(techs)
-    shuffleCards(cards)
-
-    initializeCards(cards)
-
+    initializeCards(game.createCardsFromTechs())
 }
 
-function initializeCards(cards){
+//Função responsável por criar as cartas
+function initializeCards(cards) {
     let gameBoard = document.getElementById("gameBoard")
-    
-    cards.forEach(card =>{
+
+    game.cards.forEach(card => {
         let cardElement = document.createElement('div');
         cardElement.id = card.id
         cardElement.classList.add(CARD)
@@ -41,94 +24,51 @@ function initializeCards(cards){
 
         createCardContent(card, cardElement)
 
-        cardElement.addEventListener('click',flipCard)
+        cardElement.addEventListener('click', flipCard)
         gameBoard.appendChild(cardElement)
     })
 
 }
 
-
-
-
-function createCardContent(card, cardElement){
+//Função responsável por criar o content de cada carta
+function createCardContent(card, cardElement) {
     createCardFace(FRONT, card, cardElement)
     createCardFace(BACK, card, cardElement)
 }
 
-
-function createCardFace(face, card, element){
+//Função responsável por criar a parte da frente e de trás da carta
+function createCardFace(face, card, element) {
     let cardElementFace = document.createElement('div')
     cardElementFace.classList.add(face)
-    if(face == FRONT){
+    if (face == FRONT) {
         let iconElement = document.createElement('img');
         iconElement.classList.add(ICON);
         iconElement.src = "./imagens/" + card.icon + ".png"
         cardElementFace.appendChild(iconElement)
-    }else{
+    } else {
         cardElementFace.innerHTML = '&lt/&gt'
     }
 
     element.appendChild(cardElementFace)
 }
 
+function flipCard() {
 
+    if (game.setCard(this.id)) {
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id)
+                    let secondCardView = document.getElementById(game.secondCard.id)
 
-//Função para embaralhamento das cartas, contendo a lógica
-//selecionando a ultima carta o index dela, e trocando por outra carta aleatória.
-
-function shuffleCards(cards) {
-    let currentIndex = cards.length;
-    let randomIndex = 0;
-
-    while (currentIndex !== 0) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+                    firstCardView.classList.remove('flip')
+                    secondCardView.classList.remove('flip')
+                    game.unflipCards()
+                }, 1000)
+            }
+        }
     }
-}
-
-// modelo para cada carta carta, que deve ter o icone.  ICON
-// precisa saber se a carta está virada ou não. FLIPPED
-// diferenciar uma carta da outra, mesmo que sejam iguais, devem ter ids diferentes ID
-
-//Função criar cartas por tecnologia.
-function createCardsFromTechs(techs) {
-    let cards = [];
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech))
-    })
-    return cards.flatMap(pair => pair)
-}
-
-
-//Função que vai retornar o array com o par de 2 objetos contendo...
-// ID(RANDOMICO), ICON(tecnologia que o objeto representa) E FLIPPED(Começa sempre false)
-
-
-function createPairFromTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    },
-
-    {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-//Função responsável por criar um ID randomico, concatenando o nome da tecnologia
-//Com um número aleatório inteiro entre 0 e 1 mil
-
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000)
-}
-
-function flipCard(){
-    this.classList.add("flip")
 }
